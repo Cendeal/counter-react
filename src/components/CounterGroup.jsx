2387f2ddad4f5584421676bc8b28d5c8224c60f9
Cendeal <5205093You>
2020-08-03 15:17:19 +0800

    [counter][cendeal]refactor:delete unused state

--- src/components/CounterGroup.jsx
+++ src/components/CounterGroup.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import Counter from './Counter'
 
 class CounterGroup extends React.Component {
-    constructor(props) {
-        super(props)
-    }
     render() {
         return new Array(this.props.total).fill(0).map((value, index) => <Counter key={index}/>)
     }
