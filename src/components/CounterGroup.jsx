ddbd27735706d446486cafe5bb76beb4e14f7f27
Cendeal <5205093You>
2020-08-03 15:59:42 +0800

    [counter][cendeal]feature:add input

--- src/components/CounterGroup.jsx
+++ src/components/CounterGroup.jsx
@@ -2,8 +2,40 @@ import React from 'react'
 import Counter from './Counter'
 
 class CounterGroup extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            total: this.props.total,
+            components: new Array(this.props.total).fill(0)
+        }
+    }
+
+    changeInput = (e) => {
+        let value = 0
+        if (e.target.value.match(/[0-9]+/g)) {
+            value = parseInt(e.target.value)
+        }
+        this.setState({
+            total: value
+        })
+
+    }
+    confirmInput = () => {
+        this.setState({
+            components: new Array(this.state.total).fill(0)
+        })
+    }
+
+    generateCounter = () => {
+        return this.state.components.map((value, index) => <Counter key={index}/>)
+    }
+
     render() {
-        return new Array(this.props.total).fill(0).map((value, index) => <Counter key={index}/>)
+        return <div>
+            <input value={this.state.total} onChange={this.changeInput}/>
+            <button onClick={this.confirmInput}>确定</button>
+            {this.generateCounter()}
+        </div>
     }
 }
 
