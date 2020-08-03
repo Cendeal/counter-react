4db341b7a1dc124244873b41e1c8648d3c958501
Cendeal <5205093You>
2020-08-03 15:03:45 +0800

    [counter][cendeal]feature:add Counter.jsx

---
+++ src/components/Counter.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            number: 0
+        }
+    }
+    increase = () => {
+        this.setState({
+            number: this.state.number + 1
+        })
+    }
+    descrease = () => {
+        this.setState({
+            number: this.state.number - 1
+        })
+    }
+    render() {
+        return <div>
+            <button onClick={this.increase}>+</button>
+            <mark>{this.state.number}</mark>
+            <button onClick={this.descrease}>-</button>
+        </div>
+    }
+}
+
+export default Counter
